4da3091f91aeecf08aae1c7b78a261e3d77d9119
hanashabrina <hanashabrinaa@gmail.com>
2023-12-01 00:20:37 +0700

    desain

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+import pickle
 from flask import Flask, render_template, request
+import numpy as np
 import joblib
 
 app = Flask(__name__)
@@ -11,43 +13,28 @@ def home():
     return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
-def predict(predict_list):
-    prediction = np.array(predict_list).reshape(1,3) # Karena inputnya 3
-    load = pickle.load(open('model.pkl','rb'))
-    result = load.predict(prediction)
-    return result
-    # # Get input from the form
-    # features = [float(x) for x in request.form.values()]
-
-    # # Make predictions using the ML model
-    # prediction = model.predict([features])[0]
-
-    # return render_template('index.html', prediction=prediction)
-
-def result():
+def predict():
     if request.method == 'POST':
-        name = request.form['name']
-        gender = request.form['gender']
-        age = request.form['age']
-        annual_income = request.form['annual_income']
-        spending_score = request.form['spending_score']
+        # Get input from the form
+        age = int(request.form['age'])
+        annual_income = int(request.form['annual_income'])
+        spending_score = int(request.form['spending_score'])
 
-        predict_list = list(map(int, [age, annual_income, spending_score]))
-        result = predict(predict_list)
+        # Make a prediction using the ML model
+        prediction = model.predict([[age, annual_income, spending_score]])[0]
 
-        if int(result) == 0:
-            prediction = 'Kamu adalah orang yang sangat boros'
-        elif int(result) == 1:
-            prediction = 'Kamu adalah orang yang boros'
-        elif int(result) == 2:
-            prediction = 'Kamu adalah orang yang hemat'
-        elif int(result) == 3:
-            prediction = 'Kamu sederhana'
-        elif int(result) == 4:
-            prediction = 'Kamu seorang mahasiswa'
+        if prediction == 0:
+            prediction_text = 'Kamu adalah orang yang sangat boros'
+        elif prediction == 1:
+            prediction_text = 'Kamu adalah orang yang boros'
+        elif prediction == 2:
+            prediction_text = 'Kamu adalah orang yang hemat'
+        elif prediction == 3:
+            prediction_text = 'Kamu sederhana'
+        elif prediction == 4:
+            prediction_text = 'Kamu seorang mahasiswa'
 
-        return render_template('index.html', prediction=result)
-
+        return render_template('index.html', prediction=prediction_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
